7d8ef070aa69f02c8b0e39e033eed279c0976cef
Marc Binder <marc.binder@boerse-go.de>
2015-11-25 18:42:16 +0100

    Move forward for a new version (using codacy nativly, disable verbose mode by default)

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 var pluginName = 'gulp-codacy';
-var util = require('util');
 var gutil = require('gulp-util');
 var through2 = require('through2');
-var exec = require('mz/child_process').exec;
 var merge = require('lodash.merge');
-var fs = require('fs');
-var path = require('path');
+var codacy = require('codacy-coverage/lib/handleInput');
 
 module.exports = function ccm(options) {
-  var executablePath = path.join(__dirname, 'node_modules', '.bin', 'codacy-coverage');
-
-  try {
-    fs.lstatSync(executablePath);
-  } catch (err) {
-    executablePath = path.join(__dirname, '../', '.bin', 'codacy-coverage');
-  }
-
   options = merge({
     token: null,
-    executable: executablePath,
-    verbose: true
+    commit: null,
+    format: 'lcov',
+    prefix: '',
+    verbose: false
   }, options || {});
 
-  return through2.obj(function handleFile(file, encoding, callback) {
+  return through2.obj(function handleItem(item, encoding, callback) {
     var stream = this;
 
-    if (file.isStream()) {
+    if (item.isStream()) {
       stream.emit('error', new gutil.PluginError({
         plugin: pluginName,
         message: 'Streams are not supported.'
@@ -34,30 +25,51 @@ module.exports = function ccm(options) {
       return callback();
     }
 
-    exec(util.format('CODACY_PROJECT_TOKEN=%s %s < "%s"', options.token, options.executable, file.path))
-      .then(function execCompleted(stdout, stderr) {
-        if (stderr) {
-          stream.emit('error', new gutil.PluginError({
-            plugin: pluginName,
-            message: stderr
-          }));
-
-          return callback();
-        }
-
+    codacy(item.contents.toString(), options)
+      .then(function handleCodacyResult() {
+        // TODO: gulp-istanbul should ignore this if statement block
         if (options.verbose) {
-          gutil.log('Coverage file posted: "%s"', file.path);
+          gutil.log('Coverage file posted: "%s"', item.path);
         }
 
         stream.emit('end');
       })
-      .catch(function throwPluginError(err) {
+      .catch(function handleError(err) {
         stream.emit('error', new gutil.PluginError({
           plugin: pluginName,
-          message: err.message
+          message: err.message,
+          stack: err.stack
         }));
 
         callback();
-      });
+      })
+    ;
+
+    // exec(util.format('CODACY_PROJECT_TOKEN=%s %s < "%s"', options.token, options.executable, file.path))
+    //   .then(function execCompleted(stdout, stderr) {
+    //     if (stderr) {
+    //       stream.emit('error', new gutil.PluginError({
+    //         plugin: pluginName,
+    //         message: stderr
+    //       }));
+    //
+    //       return callback();
+    //     }
+    //
+    //     if (options.verbose) {
+    //       gutil.log('Coverage file posted: "%s"', file.path);
+    //     }
+    //
+    //     stream.emit('end');
+    //   })
+    //   .catch(function throwPluginError(err) {
+    //     stream.emit('error', new gutil.PluginError({
+    //       plugin: pluginName,
+    //       message: err.message
+    //     }));
+    //
+    //     callback();
+    //   })
+    // ;
   });
 };
--- test/index.test.js
+++ test/index.test.js
@@ -3,39 +3,73 @@ var util = require('gulp-util');
 var array = require('stream-array');
 var File = util.File;
 
-function stream(file) {
-  return array([file || new File({
-    cwd: process.cwd(),
-    base: '/test',
-    path: 'test.lcov',
-    contents: new Buffer('')
-  })]);
-}
-
 describe('gulp-codacy', function gulpCodacyTestSuite() {
-  it('should throw an error if one of the obtained stream files does not exist', function noContent(done) {
-    stream()
-      .pipe(codacy())
-      .on('error', function expectedError() {
-        done();
-      })
-      .on('end', function handleEnd() {
-        done(new Error('Wrong event'));
-      })
-    ;
-  });
-
-  it('should support path with spaces', function withSpaces(done) {
+  it('rejects if the required token is not provided', function withSpaces(done) {
     var file = new File({
       cwd: process.cwd(),
       path: 'test/coverage-with-spaces.lcov',
       contents: new Buffer('')
     });
 
-    stream(file)
-      .pipe(codacy())
-      .on('error', done)
-      .on('end', done)
+    array([file])
+      .pipe(codacy({}))
+      .on('error', function handleExpectedError() {
+        done();
+      })
+      .on('end', function failWithoutError() {
+        done(new Error('Expect error'));
+      })
     ;
   });
+
+  it('rejects if the obtained file doesnt contain content', function noContentTest(done) {
+    var file = new File({
+      cwd: process.cwd(),
+      path: 'path/to/somewhere.lcov',
+      contents: new Buffer('')
+    });
+
+    array([file])
+      .pipe(codacy({ token: '...' }))
+      .on('error', function handleExpectedError() {
+        done();
+      })
+      .on('end', function handleEnd() {
+        done(new Error('Expect error'));
+      })
+    ;
+  });
+
+  it('should submit the obtained coverage', function successTest(done) {
+    var file = new File({
+      cwd: process.cwd(),
+      path: 'path/to/somewhere.lcov',
+      contents: new Buffer(['TN:gulp-coverage output', 'TN:', 'SF:./src/foo/bar.js', 'end_of_record'].join('\n'))
+    });
+
+    array([file])
+      .pipe(codacy({
+        token: '...'
+      }))
+      .on('error', function handleError(err) {
+        done(err);
+      })
+      .on('end', function handleEnd() {
+        done();
+      })
+    ;
+  });
+
+  // use vinyl file stream instead of stream-array
+  // it('does not support streams', function noStreamsTest(done) {
+  //   array([array([])])
+  //     .pipe(codacy())
+  //     .on('error', function handleExpectedError() {
+  //       done();
+  //     })
+  //     .on('end', function failWithoutError() {
+  //       done(new Error('Expect error'));
+  //     })
+  //   ;
+  // });
 });
